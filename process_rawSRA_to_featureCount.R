1e0de59f8159ae2395928618eb4771a946f9c5b4
Leroy Bondhus <bondh185@g.ucla.edu>
2022-11-02 12:41:15 -0700

    cleaning for pipeline setup

--- process_rawSRA_to_featureCount.R
+++ process_rawSRA_to_featureCount.R
@@ -1,36 +1,50 @@
+## author: Leroy Bondhus
+## date: 2022 11 2
+
+#### MANUAL PREP STEP ####
+### SraExpPack_{SRPID}.xml files downloaded manually
+### Search for SRP associated with relevant GEO or SRA in NCBI SRA
+### Click 'Sent to:' button on top right of result page
+### Select 'Full XML' and click 'Create File' button
+### Move downloaded 'SraExperimentPackage.xml' to './data/sra_metadata/SraExpPack_{SRPID}.xml'
+
 ### read in xml of data
 library(XML)
 
-## make loop IF way of accessing relevant data pieces is standard ELSE keep SRP by SRP approach
-# xml_files <- list.files("./data/sra_metadata",full.names = T)
-# xml_files <- xml_files[grep("\\.xml",xml_files)]
-# for(f in xml_files){
-#   temp <- xmlToList(f)
-# }
+dirs <- list(data="./data/",
+             sra_metadata="./data/sra/metadata/",
+             scratch="$SCRATCH/")
+ 
 
+##### 
 if(file.exists("./data/sra_metadata/SraExpPack_SRP018525.xml")){
   root_sras <- xmlRoot(xmlParse("./data/sra_metadata/SraExpPack_SRP018525.xml"))
   # temp <- xmlToList(root_sras[[1]])
   sras <- xmlToList("./data/sra_metadata/SraExpPack_SRP018525.xml")
-  temp_cols <- c("srp_id","srr_id","taxon_id","file_size","sample_title", "sample_age","source")
+  temp_cols <- c("srp_id","srr_id","taxon_id","file_size","sample_title", "std_dev_time","source","library_info")
   sra_metadata <- data.frame(matrix(nrow=length(sras),ncol=length(temp_cols))) 
   colnames(sra_metadata) <- temp_cols
   for(i in 1:length(sras)){
+    temp_df <- cbind(xmlToDataFrame(xmlElementsByTagName(root_sras[[i]],"TAG", recursive=TRUE)),
+                     xmlToDataFrame( xmlElementsByTagName(root_sras[[i]],"VALUE", recursive=TRUE)))
+    colnames(temp_df) <- c("TAG","VALUE")
+    
     sra_metadata$srp_id[i] <- sras[[i]]$EXPERIMENT$STUDY_REF$IDENTIFIERS$PRIMARY_ID
     sra_metadata$srr_id[i] <- sras[[i]]$RUN_SET$RUN$IDENTIFIERS$PRIMARY_ID
     sra_metadata$taxon_id[i] <- sras[[i]]$SAMPLE$SAMPLE_NAME$TAXON_ID
     sra_metadata$file_size[i] <- sras[[i]]$RUN_SET$RUN$.attrs[["size"]] ## file size
-    # utils:::format.object_size(as.numeric( sras[[i]]$RUN_SET$RUN$.attrs["size"]),"Mb")
     sra_metadata$sample_title[i] <- sras[[i]]$SAMPLE$TITLE
-    
-    temp_df <- cbind(xmlToDataFrame(xmlElementsByTagName(root_sras[[i]],"TAG", recursive=TRUE)),
-                     xmlToDataFrame( xmlElementsByTagName(root_sras[[i]],"VALUE", recursive=TRUE)))
-    colnames(temp_df) <- c("TAG","VALUE")
     sra_metadata$source[i] <- temp_df$VALUE[which(temp_df$TAG == "source_name")]
+    
+    sra_metadata$library_info[i] <- paste0(c(as.character(sras[[1]]$EXPERIMENT$DESIGN$LIBRARY_DESCRIPTOR[c(1,3)]),
+                                        names(sras[[1]]$EXPERIMENT$DESIGN$LIBRARY_DESCRIPTOR$LIBRARY_LAYOUT)),
+                                      collapse = "__")
   }
   utils:::format.object_size(sum(as.numeric(sra_metadata$file_size) ), "Gb")
 }
 
+
+## fasterq-dump -> fastq -> bam -> featureCount
 
 
 # -> loop through 
